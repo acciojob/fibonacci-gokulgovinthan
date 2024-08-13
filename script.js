15a0bff8bd8043c11a2e4588c6059f119a5f9458
gokulgovinthan <goodgovinthan@gmail.com>
2024-08-13 12:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
-// your code here
+ const arr= [0,1]
+	for(let i=2;i<=num;i++){
+		arr[i]=arr[i-1]+arr[i-2]
+	}
+	return arr[num-1]
 }
+
+console.log(fibonacci(5))
 
 module.exports = fibonacci;
